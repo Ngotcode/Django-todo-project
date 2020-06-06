51e861f86411e8b1f16ead7a3e195acae1fd541a
Teri Ngo <ngot@janelia.hhmi.org>
2020-06-06 17:00:32 -0400

    "Add/update todos models.py files

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)        #to show as viewable in django Admin database
+
+#admin.site.register(Todo)
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)       #this is un-editable but need to see it in the Admin database, seee admin.py
+    datecompleted = models.DateTimeField(null=True, blank=True)      #date/time units designated null as compare to blank
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    #need this to distinguish between different users' todos app
+
+
+#name model objects 
+    def __str__(self):
+        return self.title
