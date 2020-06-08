391a369751d420a55c25b4fa3839b92b39870abe
Teri Ngo <ngot@janelia.hhmi.org>
2020-06-07 22:06:28 -0400

    "Add todo list to current in views.py"

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 def home(request):
@@ -63,4 +64,7 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Bad data passed in. Try again.'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    #todos: Todo.objects.all()        #this list todos for all users
+    return render(request, 'todo/currenttodos.html', {'todos':todos})
+
